e5308dcf422ca1a56a615a48a91746ea051ea93d
Dillon Rochford <dillonrochford@gmail.com>
2020-10-15 16:41:42 +0100

    Add product text fields

--- app/src/main/java/com/example/marketswipe/activities/AddProductActivity.java
+++ app/src/main/java/com/example/marketswipe/activities/AddProductActivity.java
@@ -1,16 +1,43 @@
 package com.example.marketswipe.activities;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.marketswipe.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddProductActivity extends AppCompatActivity {
+
+    private DatabaseReference categoriesDB;
+    private Spinner catSpinner, subCatSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addproduct);
+
+        catSpinner = findViewById(R.id.cat_spinner);
+        subCatSpinner = findViewById(R.id.subcat_spinner);
+
+        categoriesDB = FirebaseDatabase.getInstance().getReference("Categories");
+        categoriesDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/marketswipe/activities/SignInActivity.java
+++ app/src/main/java/com/example/marketswipe/activities/SignInActivity.java
@@ -29,10 +29,21 @@ public class SignInActivity extends AppCompatActivity {
     private Button registerButton;
     private TextView orSignInText;
 
+    private Button testProduct;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        testProduct = findViewById(R.id.testAddProduct);
+        testProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this, AddProductActivity.class);
+                startActivity(intent);
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
 
